35d314893f817ff018751ea36fe4285d7752d3ca
Matt Hinczuk <matiasmoya@gmail.com>
2016-10-15 13:09:28 -0300

    returns string for messages 'from' attr in the ticket response

--- app/serializers/ticket_message_serializer.rb
+++ app/serializers/ticket_message_serializer.rb
@@ -2,7 +2,7 @@ class TicketMessageSerializer < ActiveModel::Serializer
   attributes :id, :content, :from
 
   def from
-    "Admin" if object.admin.present?
-    "You" if object.user == scope
+    return "Admin" if object.admin.present?
+    return "You" if object.user == scope
   end
 end
